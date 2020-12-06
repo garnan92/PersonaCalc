ad61280d03cb71903b083bad01c57ef38c262ed7
garnan92 <garnan92@gmail.com>
2020-12-06 17:34:36 -0600

    creacion de tabs

--- PersonaCalc/LoginViewController.swift
+++ PersonaCalc/LoginViewController.swift
@@ -7,12 +7,28 @@
 
 import Foundation
 import UIKit
+import TextFieldFloatingPlaceholder
 
 class LoginViewController : UIViewController {
     
+    @IBOutlet weak var userTextField: TextFieldFloatingPlaceholder!
+    @IBOutlet weak var passwordTextField: TextFieldFloatingPlaceholder!
+    
+    private let loginPresenter: LoginPresenter = LoginPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginPresenter.setTextfieldProperties(userTextField)
+        loginPresenter.setTextfieldProperties(passwordTextField)
+        loginPresenter.setController(self)
+    }
+    
+    @IBAction func entrarDidLogin(_ sender: Any) {
+        loginPresenter.LogIn(userTextField, passwordTextField)
+    }
+    
+    @IBAction func registrarseDidPressed(_ sender: Any) {
+        print("se quiere registrar")
     }
     
     
